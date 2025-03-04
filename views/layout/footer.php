261473f8a920d76ff71161d977b3c5495434176a
ign-is <igna.dev.contact@gmail.com>
2025-03-04 16:13:24 -0300

    Add footer.php

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+    
+    <!-- PIE DE PAGINA -->
+     <footer id="footer">
+        <p>Desarrollado por Joe Doe WEB &copy; <?=date('Y')?></p>
+     </footer>
+</div>
+</body>
+</html>
