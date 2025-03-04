749c6c8c1f6fd76dfa6dc2819b2dbe5e81c6945f
ign-is <igna.dev.contact@gmail.com>
2025-03-04 16:13:02 -0300

    Add header.php

---
+++ views/layout/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?=base_url?>assets/css/styles.css" />
+    <title>Tienda de Camisetas</title>
+</head>
+<body>
+    <div id="container">
+        <!-- CABECERA -->
+        <header id="header">
+            <div class="logo" id="logo">
+                <img src="<?=base_url?>assets/img/camiseta.png" alt="Camiseta Logo">
+                <a href="index.php">
+                    Tienda de camisetas
+                </a>
+            </div>
+        </header>
+
+        <!-- MENU -->
+         <?php $categorias = Utils::showCategorias(); ?>
+        <nav id="menu">
+            <ul>
+                <li>
+                    <a href="<?=base_url?>">Inicio</a>
+                </li>
+                <?php while($cat = $categorias->fetch_object()): ?>
+                    <li>
+                        <a href="<?=base_url?>categoria/ver&id=<?=$cat->id?>"><?=$cat->nombre?></a>
+                    </li>
+                <?php endwhile; ?>
+            </ul>
+        </nav>
+    
+        <div id="content">
