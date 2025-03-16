7cb3a591672275a88aeaa0fbd38e6ed56276675d
ign-is <igna.dev.contact@gmail.com>
2025-03-15 22:07:48 -0300

    Add utils.php update

--- helpers/utils.php
+++ helpers/utils.php
@@ -1,31 +1,39 @@
 <?php
 
-class Utils {
-   
-    public static function deleteSession($name) {
-        if(isset($_SESSION[$name])) {
-            $_SESSION[$name] = null;
-            unset($_SESSION[$name]);
-        }
-
-        return $name;
-    }
-
-    public static function isAdmin() {
-        if(!isset($_SESSION['admin'])) {
-            header("Location:".base_url);
-        } else {
-            return true;
-        }
-    }
-
-    public static function showCategorias() {
-        require_once 'models/categoria.php';
-        $categoria = new Categoria();
-        $categorias = $categoria->getAll();
-        return $categorias;
-    }
-
+class Utils{
+	
+	public static function deleteSession($name){
+		if(isset($_SESSION[$name])){
+			$_SESSION[$name] = null;
+			unset($_SESSION[$name]);
+		}
+		
+		return $name;
+	}
+	
+	public static function isAdmin(){
+		if(!isset($_SESSION['admin'])){
+			header("Location:".base_url);
+		}else{
+			return true;
+		}
+	}
+	
+	public static function isIdentity(){
+		if(!isset($_SESSION['identity'])){
+			header("Location:".base_url);
+		}else{
+			return true;
+		}
+	}
+	
+	public static function showCategorias(){
+		require_once 'models/categoria.php';
+		$categoria = new Categoria();
+		$categorias = $categoria->getAll();
+		return $categorias;
+	}
+	
 	public static function statsCarrito(){
 		$stats = array(
 			'count' => 0,
@@ -42,4 +50,21 @@ class Utils {
 		
 		return $stats;
 	}
+	
+	public static function showStatus($status){
+		$value = 'Pendiente';
+		
+		if($status == 'confirm'){
+			$value = 'Pendiente';
+		}elseif($status == 'preparation'){
+			$value = 'En preparación';
+		}elseif($status == 'ready'){
+			$value = 'Preparado para enviar';
+		}elseif($status = 'sended'){
+			$value = 'Enviado';
+		}
+		
+		return $value;
+	}
+	
 }
