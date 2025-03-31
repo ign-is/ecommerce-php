f74916a31d30e74a6dcd64ce07ded4e6cc0a9933
ign-is <igna.dev.contact@gmail.com>
2025-03-31 18:16:12 -0300

    Add views carrito index.php update

--- views/carrito/index.php
+++ views/carrito/index.php
@@ -1,6 +1,6 @@
 <h1>Carrito de la compra</h1>
 
-<?php //if(isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1): ?>
+<?php if(isset($_SESSION['carrito']) && count($_SESSION['carrito']) >= 1): ?>
 <table>
 	<tr>
 		<th>Imagen</th>
@@ -43,8 +43,15 @@
 	<?php endforeach; ?>
 </table>
 <br/>
+<div class="delete-carrito">
+	<a href="<?=base_url?>carrito/delete_all" class="button button-delete button-red">Vaciar carrito</a>
+</div>
 <div class="total-carrito">
 	<?php $stats = Utils::statsCarrito(); ?>
 	<h3>Precio total: <?=$stats['total']?> $</h3>
 	<a href="<?=base_url?>pedido/hacer" class="button button-pedido">Hacer pedido</a>
 </div>
+
+<?php else: ?>
+	<p>El carrito está vacio, añade algun producto</p>
+<?php endif; ?>
